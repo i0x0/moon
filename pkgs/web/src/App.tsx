91e18f54c17fedfc273841cc9797edba14711843
TheHackerCoding <triniziah@gmail.com>
2022-03-03 00:00:30 -0500

    uhh i fixed something

--- pkgs/web/src/App.tsx
+++ pkgs/web/src/App.tsx
@@ -1,9 +1,14 @@
-import SideBar from "./Components/SideBar";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <div className="flex">
       <SideBar />
+      <div className="m-0 ml-16 w-full">
+        <div className="grid place-items-center h-screen overflow-hidden">
+          <h1>Helwo</h1>
+        </div>
+      </div>
     </div>
   );
 }
