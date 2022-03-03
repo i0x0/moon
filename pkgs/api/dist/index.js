0e45d8a3da348791f942d06841fb7fc454ec1a79
TheHackerCoding <triniziah@gmail.com>
2022-03-02 22:19:49 -0500

    uhh i redid everything

---
+++ pkgs/api/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Server_1 = __importDefault(require("./Server"));
+new Server_1.default();
+//# sourceMappingURL=index.js.map
---
+++ pkgs/web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center">helwo ppl</h1>
+    </div>
+  );
+}
+
+export default App;
